f89d69165fd685a745145ca32cb86d1cee7cdc9f
ianramy <imwagore@gmail.com>
2024-06-26 04:26:18 +0300

    origin/master

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+// 1. Variable `multiply`
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+// 2. Variable `random`
+let random = Math.ceil(Math.random() * 100); 
+
+// 3. Variable `mod`
+const num3 = 64;
+const num4 = 60;
+let mod = num3 % num4; 
+
+// 4. Variable `max`
+let max = Math.max(5, 10, 15, 20); 
+
+console.log('multiply:', multiply); 
+console.log('random:', random); 
+console.log('mod:', mod); 
+console.log('max:', max); 
